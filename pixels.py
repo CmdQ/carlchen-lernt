7e217c47a958dc47bb4ee8b65c711a67cb868a5e
Tobias Becker <nullpointeracception@gmail.com>
2021-07-21 20:23:09 +0200

    Alpha-Blending gemacht

--- pixels.py
+++ pixels.py
@@ -15,3 +15,33 @@ def forgetRed(pixels):
 #
 # rgbrgbrgbrgb # interleaved
 # rrrrggggbbbb # kanalweise
+
+
+# Assume RGBA tuples
+def alpha_blend(upper, lower):
+    height = len(lower)
+    combined = []
+    for row in range(height):
+        width = len(lower[row])
+        combined_row = []
+        for col in range(width):
+            r, g, b, alpha = upper[row][col]
+            lr, lg, lb, lalpha = lower[row][col]
+            ialpha = 1.0 - alpha
+            combined_row.append((
+                alpha * r + ialpha * lalpha * lr,
+                alpha * g + ialpha * lalpha * lg,
+                alpha * b + ialpha * lalpha * lb,
+                1.0))
+        combined.append(combined_row)
+    return combined
+
+class RGBA:
+    def __init__(self, red, green, blue, alpha=1.0) -> None:
+        self.red = red
+        self.green = green
+        self.blue = blue
+        self.alpha = alpha
+
+    def __repr__(self) -> str:
+        return f"RGBA({self.red}, {self.green}, {self.blue}, {self.alpha})"
